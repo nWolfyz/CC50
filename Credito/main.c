8d062c14ee0361d97b09e3d30eabbd3a08a758ec
nWolfyz <nicolaslobo9@gmail.com>
2024-08-14 18:27:51 -0300

    commit

--- Credito/main.c
+++ Credito/main.c
@@ -6,6 +6,6 @@ int main(void)
 {
     //Armazenar número do cartão
     long card_number = get_long("Insira o número do cartão: ");
-    printf("O cartão inserido é: %ld\n", card_number);
+    printf("O cartã inserido é: %ld\n", card_number);
 
 }
